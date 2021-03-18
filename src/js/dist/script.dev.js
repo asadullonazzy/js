ae1f9a77eec2ab0698ce1d85f12cc1c0798303df
asadullo <asadullonazarov904@gmail.com>
2021-03-18 15:37:11 +0500

    first commit

---
+++ src/js/dist/script.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+"use sctrict"; // let number = 4.6;
+
+console.log(4 / 0);
+console.log("string" * 9);
+var persone = "alex";
+var bool = true;
+console.log(something);
+var und;
+console.log(und);
+var obj = {
+  name: "john",
+  age: 25,
+  isMarried: false
+};
+console.log(obj.age);
+var arr = ["plum.png", "orange.jpg", 4.5, "apple.jpg", {}, []];
+console.log(arr[1]);
---
+++ src/js/script.js
@@ -0,0 +1,91 @@
+"use strict";
+// ................................
+console.log("arr" + "-object");
+console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+// ......
+console.log(5 % 2);
+
+console.log(2 * 4 == 8);
+
+// ... &&- и
+// ....||-ИЛИ
+
+const isChecked = true,
+  isClose = false;
+
+console.log(isChecked && isClose);
+
+// .................................
+
+// alert("hello");
+
+// const result = confirm("are you here");
+// console.log(result);
+
+// const answer = +prompt("вам есть 18", "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// // answers[0] = prompt("как ваше имя ", "");
+// // answers[1] = prompt("как ваше фамилия ", "");
+// // answers[2] = prompt("сколько вам лет ", "");
+
+// console.log(typeof answers);
+// (`as;d${gd}`)
+
+// const category = "toys";
+
+// console.log(`https//someurl.com/${category}/5`);
+
+// const user = "asad";
+
+// alert(`привет, ${user}`);
+
+// var a = 5;
+// var b = 10;
+// console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
+
+// let number = 4.6;
+// function run(t) {
+//   console.log(t);
+// }
+// run(array);
+// let obj = {};
+// const number = "some";
+// var array = "some";
+// let bool = 45;
+
+// obj = 36;
+
+// console.log(4 / 0);
+
+// console.log("string" * 9);
+
+// const persone = "alex";
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//   name: "john",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// console.log(obj.age);
+
+// let arr = ["plum.png", "orange.jpg", 4.5, "apple.jpg", {}, []];
+// console.log(arr[1]);
