9b94eb447040e99d776c5e8bd8141c4578c9e35c
asadullo <asadullonazarov904@gmail.com>
2021-03-18 15:52:41 +0500

    first second

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@ console.log(2 * 4 == 8);
 const isChecked = true,
   isClose = false;
 
-console.log(isChecked && isClose);
+console.log(isChecked || isClose);
 
 // .................................
 
