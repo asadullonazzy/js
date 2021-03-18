bf29baf2baf95aad57e97ae503419b7391a01085
Asadullo <asadullonazarov904@gmail.com>
2021-03-18 16:46:26 +0500

    git add -a -m"news"

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,27 @@
 "use strict";
 // ................................
-console.log("arr" + "-object");
-console.log(4 + +"5");
+// console.log("arr" + "-object");
+// console.log(4 + +"5");
 
-let incr = 10,
-  decr = 10;
+// let incr = 10,
+//   decr = 10;
 
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
-// ......
-console.log(5 % 2);
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+// // ......
+// console.log(5 % 2);
 
-console.log(2 * 4 == 8);
+// console.log(2 * 4 == 8);
 
-// ... &&- и
-// ....||-ИЛИ
+// // ... &&- и
+// // ....||-ИЛИ
 
-const isChecked = true,
-  isClose = false;
+// const isChecked = true,
+//   isClose = false;
 
-console.log(isChecked || isClose);
+// console.log(isChecked || isClose);
 
 // .................................
 
